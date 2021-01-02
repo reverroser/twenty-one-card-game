d0968fa2c8397726287a64736f80b112649b6599
Roser Reverte <reverroser@gmail.com>
2021-01-02 17:56:29 +0000

    Create game area

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 
 const HIGH_SCORE_KEY = 'high_score';
 
+const DECK_API_URL = 'https://deckofcardsapi.com/api/deck';
+
 function App() {
   const [highScore, setHighScore] = useState(localStorage.getItem(HIGH_SCORE_KEY) || 0);
+  const [cards, setCards] = useState([]);
+  const [deckId, setDeckId] = useState();
+
+  useEffect(() => {
+    fetch(`${DECK_API_URL}/new/`)
+      .then((response) => response.json())
+      .then(({ deck_id }) => setDeckId(deck_id));
+  }, []);
 
   return (
     <div className="app">
@@ -16,6 +26,9 @@ function App() {
         <div className="score">
           High Score: <span>{highScore}</span>
         </div>
+      </div>
+      <div className="gameArea">
+        {cards.map((card, i) => <img key={i} src={card.image} alt={card.code} />)}
       </div>
     </div>
   );
