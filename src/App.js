7b2c225f0b747c33e50f4f795f58d43b2418d92f
Roser Reverte <reverroser@gmail.com>
2021-01-02 17:22:41 +0000

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
+const HIGH_SCORE_KEY = 'high_score';
+
 function App() {
+  const [highScore, setHighScore] = useState(localStorage.getItem(HIGH_SCORE_KEY) || 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <div className="logo">
+          21 Game
+        </div>
+        <div className="score">
+          High Score: <span>{highScore}</span>
+        </div>
+      </div>
     </div>
   );
 }
